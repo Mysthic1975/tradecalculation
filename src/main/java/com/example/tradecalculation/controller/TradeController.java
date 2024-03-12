23ab292ab45520e66ca9cd2f2a5c284a334b286d
Marcel Rohrbach <MarcelRohrbach@yahoo.de>
2024-03-12 13:10:40 +0100

    Controller change

--- src/main/java/com/example/tradecalculation/controller/TradeController.java
+++ src/main/java/com/example/tradecalculation/controller/TradeController.java
@@ -1,3 +1,5 @@
+package com.example.tradecalculation.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
