bad180c53457d233c4b95612b6c1d11bfd54fe95
Marcel Rohrbach <MarcelRohrbach@yahoo.de>
2024-03-12 11:31:47 +0100

    Handelskalkulation WebApp

---
+++ src/main/java/com/example/tradecalculation/HandelskalkulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.tradecalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandelskalkulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandelskalkulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tradecalculation/HandelskalkulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tradecalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandelskalkulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
